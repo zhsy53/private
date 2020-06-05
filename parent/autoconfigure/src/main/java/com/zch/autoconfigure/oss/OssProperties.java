40cf406edbad27cdf7ade0aaab657d84dfe79246
zhangsanyuan <zhangsanyuan@zhongch.onaliyun.com>
2020-06-05 16:14:02 +0800

    update

--- parent/autoconfigure/src/main/java/com/zch/autoconfigure/oss/OssProperties.java
+++ parent/autoconfigure/src/main/java/com/zch/autoconfigure/oss/OssProperties.java
@@ -1,4 +1,19 @@
 package com.zch.autoconfigure.oss;
 
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import javax.validation.constraints.NotBlank;
+
+@ConfigurationProperties(prefix = "zch.oss")
+@Data
 public class OssProperties {
+    @NotBlank
+    private String url;
+    @NotBlank
+    private String bucket;
+    @NotBlank
+    private String key;
+    @NotBlank
+    private String secret;
 }
